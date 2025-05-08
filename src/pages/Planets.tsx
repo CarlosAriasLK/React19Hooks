d4ba1aa4c0abc51f7b5fb176a880e2fb990a4b53
Carlos Arias <ceau922@gmail.com>
2025-05-07 19:53:03 -0500

    React 19 new features

---
+++ src/pages/Planets.tsx
@@ -0,0 +1,30 @@
+import { FC, use } from 'react';
+
+import { Planet } from '../interfaces/planet.interface';
+import { EditPlanetForm } from './ui/EditPlanetForm';
+import { PlanetList } from './ui/PlanetList';
+
+interface Props {
+  getPlanets: Promise<Planet[]>
+}
+
+const Planets: FC<Props> = ({ getPlanets }) => {
+
+  const planets = use( getPlanets );
+
+  const handleAddPlanet = (planet: Partial<Planet>) => {
+    console.log(planet);
+  };
+
+  return (
+    <>
+      <h4 className="text-2xl font-thin mb-4">Agregar y mantener planetas</h4>
+      <hr className="border-gray-300 mb-4" />
+      {/* Formulario para agregar un planeta */}
+      <EditPlanetForm onAddPlanet={handleAddPlanet} />
+      <PlanetList planets={ planets } />
+    </>
+  );
+};
+
+export default Planets;
