fad7e218962e77a61809c52948336fa6ba5a92e7
Carlos Arias <ceau922@gmail.com>
2025-05-08 17:36:13 -0500

    add(ui) SubmitButton with new react19 hook

--- src/pages/ui/EditPlanetForm.tsx
+++ src/pages/ui/EditPlanetForm.tsx
@@ -1,6 +1,7 @@
 import { useActionState } from 'react';
 import { Planet } from '../../interfaces/planet.interface';
 import { createPlanetActionForm } from '../../actions/create-planet.action';
+import { SubmitButton } from './SubmitButton';
 
 interface Props {
   onAddPlanet: (planet: Planet) => void;
@@ -8,7 +9,7 @@ interface Props {
 
 export const EditPlanetForm = ({ onAddPlanet }: Props) => {
 
-  const [, formAction, isPending] = useActionState(
+  const [, formAction, ] = useActionState(
     async (prevState: unknown, queryData: FormData) => {
       const planet = await createPlanetActionForm(prevState, queryData);
       onAddPlanet(planet);
@@ -16,8 +17,6 @@ export const EditPlanetForm = ({ onAddPlanet }: Props) => {
     
   return (
     <form className="mb-4 flex flex-col md:flex-row" action={formAction}>
-
-      <h1>{isPending ? 'Pending' : 'No pending'}</h1>
 
       <input
         type="text"
@@ -40,13 +39,8 @@ export const EditPlanetForm = ({ onAddPlanet }: Props) => {
         name='distanceFromSun'
         required
       />
-      <button
-        type="submit"
-        className="bg-blue-500 disabled:bg-gray-500 text-white p-2 rounded flex-1 sm:flex-none"
-        disabled={isPending}
-      >
-        Agregar planeta
-      </button>
+
+      <SubmitButton />
     </form>
   );
 };
