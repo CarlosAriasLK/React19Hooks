332648493419021a968dfede2d155da5f28b3b0b
Carlos Arias <ceau922@gmail.com>
2025-05-08 17:55:38 -0500

    add(pages): useOptimistic

--- src/pages/ui/PlanetList.tsx
+++ src/pages/ui/PlanetList.tsx
@@ -1,17 +1,47 @@
+import { useOptimistic } from 'react';
 import { Planet } from '../../interfaces/planet.interface';
+import { updatePlanetAction } from '../../actions/update-planet.action';
 
 interface Props {
   planets: Planet[];
 }
 
 export const PlanetList = ({ planets }: Props) => {
+
+  const [optimisticPlanets, setOptimisticPlanets] = useOptimistic(
+    planets,
+    (current, newPlanet: Planet) => {
+      const updatedPlanets = current.map( p => p.id === newPlanet.id ? newPlanet : p);
+      
+      return updatedPlanets; 
+    }
+  )
+
+
+   const handleUpdatePlanet = async(planet: Planet) => {
+
+    planet.name = planet.name.toUpperCase();
+    setOptimisticPlanets( planet );
+    const updatedPlanet = await updatePlanetAction( planet );
+    
+
+  }
+
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 animate-fadeIn">
-      {planets.map((planet) => (
+      {optimisticPlanets.map((planet) => (
         <div key={planet.id} className="p-4 bg-gray-100 rounded shadow">
           <h2 className="text-xl font-semibold">{planet.name}</h2>
           <p className="text-gray-700">{planet.type}</p>
           <p className="text-gray-700">{planet.distanceFromSun}</p>
+
+          <br />
+          <button onClick={ () => handleUpdatePlanet( planet ) } className="bg-blue-500 text-white p-2 rounded w-full">
+            Acualizar Planeta
+          </button>
+
+
         </div>
       ))}
     </div>
